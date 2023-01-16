1692f30583b224c04d3f39510e4cbcb48c92988f
interesting1113 <sylviayang1113@gmail.com>
2023-01-16 19:41:26 +0900

    init branch

---
+++ db/query/account.sql
@@ -0,0 +1,8 @@
+-- name: CreateAccount :one
+INSERT INTO Accounts (
+  owner,
+  balance,
+  currency
+) VALUES (
+  $1, $2, $3
+) RETURNING *;
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sylviayang1113/simple-bank
+
+go 1.19
