d7dbedbf3c21311abe53ee4d296efbc66197721f
interesting1113 <sylviayang1113@gmail.com>
2023-01-19 15:55:00 +0900

    transfterTx

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -39,3 +39,56 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	}
 	return tx.Commit()
 }
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfer `json: "transfer"`
+	FromAccount Account  `json: "from_account"`
+	ToAccount   Account  `json: "to_account"`
+	FromEntry   Entry    `json: "from_entry"`
+	ToEntry     Entry    `json: "to_entry"`
+}
+
+// TransferTx performs a money transfer from one account to another
+// It creates a transfer record, add account entries, and update accounts' balance within a single database transaction
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		// TODO: update accounts' balance
+		return nil
+	})
+
+	return result, err
+}
